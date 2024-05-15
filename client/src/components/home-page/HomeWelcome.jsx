a6efe7396f96c391d4af7baeeccb4e4f5db91159
saathanac <108774535+saathanac@users.noreply.github.com>
2024-05-15 17:07:18 -0400

    minor UI changes on home page

--- client/src/components/home-page/HomeWelcome.jsx
+++ client/src/components/home-page/HomeWelcome.jsx
@@ -2,22 +2,22 @@ import React from 'react'
 import HomeGetStartedButton from './HomeGetStartedButton'
 export default function HomeWelcome() {
   return (
-    <div className="lg:justify-left w-full bg-white" style={{ marginTop: '40%' }}>
+    <div className="lg:justify-left w-full bg-white" style={{ marginTop: '33%' }}>
         <div className=" border-solid border-transparent border-[2rem]">
             <div className="text-center">
-            <p className="lg:text-3xl leading-8 text-gray-600">
-                Welcome To The
-            </p>
-            <h1 className="mt-2 lg:text-8xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                Traitlet 
-            </h1>
-            <h1 className="mt-2 lg:text-8xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                Builder 
-            </h1>
-            <p className="mt-2 lg:text-3xl leading-8 text-gray-600">
-                Create unique & customized pieces
-            </p>
-            <HomeGetStartedButton/>
+                <p className="lg:text-3xl leading-8 text-gray-600">
+                    Welcome To The
+                </p>
+                <h1 className="mt-2 lg:text-8xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                    Traitlet 
+                </h1>
+                <h1 className="mt-2 lg:text-8xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                    Builder 
+                </h1>
+                <p className="mt-2 lg:text-3xl leading-8 text-gray-600">
+                    Create unique & customized pieces
+                </p>
+                <HomeGetStartedButton/>
             </div>
         </div>
     </div>
