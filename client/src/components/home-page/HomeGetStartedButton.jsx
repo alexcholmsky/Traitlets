c20f2cca6b85d00cf17daa1cea023f88ea101b8a
saathanac <108774535+saathanac@users.noreply.github.com>
2024-05-15 17:07:18 -0400

    minor UI changes on home page

--- client/src/components/home-page/HomeGetStartedButton.jsx
+++ client/src/components/home-page/HomeGetStartedButton.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 function HomeGetStartedButton() {
   return (
-    <div className="mt-4 flex items-center justify-center gap-x-6">
+    <div className="mt-7 flex items-center justify-center gap-x-6">
         <a
             href="/builder"
-            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="rounded-md bg-indigo-600 px-10 py-4 text-lg font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
             Get started
         </a>
