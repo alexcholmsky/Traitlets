9a2ff381296b3a7012c725c37beb49a7d4db5626
saathanac <108774535+saathanac@users.noreply.github.com>
2024-05-19 10:18:27 -0400

    added feature to have icon categories

--- client/src/components/builder-page/SelectionsDisplay.jsx
+++ client/src/components/builder-page/SelectionsDisplay.jsx
@@ -13,13 +13,15 @@ import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import TextField from '@mui/material/TextField';
 import {useTheme, useMediaQuery} from '@mui/material';
+import traitletOptions from '../../traitletOptions';
 
 
 function SelectionsDisplay() {
-    const { activeStep, braceletDetails, options, addToOrder, centerpieceSide, 
+    const { activeStep, braceletDetails, options, setOptions, addToOrder, centerpieceSide, 
             setCenterpieceSide, handleEngravingChange, engravingText, addCenterpieceToOrder, type, setType, setEngravingText, backEngravingText, setBackEngravingText, handleBackEngravingChange } = useSelectionContext()
     const [value, setValue] = useState(0);
     const [isOverflow, setIsOverflow] = useState(false);
+    const [category, setCategory] = useState('zodiac-signs');
 
 
     const handleChange = (event, newValue) => {
@@ -39,9 +41,7 @@ function SelectionsDisplay() {
     };
 
     useEffect(()=> {
-        console.log("before", isOverflow)
         setIsOverflow(false)
-        console.log("after", isOverflow)
     }, [activeStep])
 
     const beadContainerRef = useRef(null);
@@ -65,26 +65,45 @@ function SelectionsDisplay() {
     }, [iconContainerRef.current, options, type, activeStep]); // Include any dependencies that might affect the container's content
 
     const overflowText = ({ container }) => {
-        console.log(container)
-        console.log("width", container.scrollWidth, container.clientWidth)
-            const handleOverflow = () => {
-                if (container.scrollWidth > container.clientWidth && (type == "icon" || activeStep != 2)) {
-                    // Container overflowed, display alert or take any other action
-                    setIsOverflow(true);
-                }
-                else {
-                    setIsOverflow(false);
-                }
-            };
+        const handleOverflow = () => {
+            if (container.scrollWidth > container.clientWidth && (type == "icon" || activeStep != 2)) {
+                // Container overflowed, display alert or take any other action
+                setIsOverflow(true);
+            }
+            else {
+                setIsOverflow(false);
+            }
+        };
 
-            // Call handleOverflow on mount and whenever the content changes
-            handleOverflow();   
+        // Call handleOverflow on mount and whenever the content changes
+        handleOverflow();   
+        }
+    
+    function formatString(str) {
+        // Split the string by dashes
+        let words = str.split('-');
+        
+        // Capitalize the first letter of each word
+        let formattedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+        
+        // Join the words back together
+        let formattedString = formattedWords.join(' ');
+        
+        return formattedString;
         }
     
     const handleChangeDesign = (event) => {
         setType(event.target.value);
         addCenterpieceToOrder(event.target.value)
     };
+
+    const handleCategoryChange = (event) => {
+        setCategory(event.target.value)
+        let value = event.target.value
+        setOptions(traitletOptions['centerpiece'][value])
+        console.log(braceletDetails)
+
+    }
     const theme = useTheme();
     const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
    
@@ -92,13 +111,6 @@ function SelectionsDisplay() {
     <div className='bg-white absolute bottom-0 w-full min-h-[50%] lg:h-[42%]'>
         <div className='w-full flex justify-center'>
             <StepNavigation/>
-        </div>
-        <div className="lg:flex justify-center mt-4 mb-4">
-            {(isOverflow && activeStep !== 2) && (
-                <div className="text-gray-500 overflow-hidden text-center text-lg transition-all duration-300 ease-in-out">
-                Scroll for more
-                </div>
-            )}
         </div>
         <div className="flex justify-center mb-10 w-full">
             
@@ -119,59 +131,83 @@ function SelectionsDisplay() {
                                 <Tab label="back-side" sx={{ width: isSmallScreen ? '150px' : '420px'}}/>
                             </Tabs>
                         </div>
-                        {isOverflow && (
+                        {/* {isOverflow && (
                         <Typography
-                        sx={{ mt: 2, mb: 1, fontSize: '14pt' }}
+                        sx={{ mt: 2, mb: 1, fontSize: '12pt' }}
                         className='text-gray-500 overflow-hidden text-center text-lg transition-all duration-300 ease-in-out'
                         >
                         {"Scroll for more"}
                         </Typography>
-                        )}
+                        )} */}
+                        <div className='flex justify-center pt-8 gap-16'>
+                            <button 
+                                className={`w-fit py-1.5 rounded-full border-2 bg-white text-gray-600  hover:border-blue-700 ${type =='icon' ? 'border-blue-400' : 'border-gray-200'}`}
+                                value='icon'
+                                onClick={handleChangeDesign}>
+                                Icon
+                            </button>
+                            <button 
+                                className={`w-fit py-1.5 rounded-full border-2 bg-white text-gray-600  hover:border-blue-700 ${type =='text' ? 'border-blue-400' : 'border-gray-200'}`}
+                                value = 'text'
+                                onClick={handleChangeDesign}>
+                                Text
+                            </button>
+                            <button 
+                                className={`w-fit py-1.5 rounded-full border-2 bg-white text-gray-600  hover:border-blue-700 ${type =='none' ? 'border-blue-400' : 'border-gray-200'}`}
+                                value = 'none'
+                                onClick={handleChangeDesign}>
+                                None
+                            </button>
+                        </div>
                         <div className='flex justify-center md:flex-row flex-col '>
-                        <Box className="flex justify-center md:w-1/6 mt-10 mr-0 lg:mr-4 px-2">
-                            <FormControl fullWidth>
-                                <InputLabel id="demo-simple-select-label">Engraving</InputLabel>
-                                <Select
-                                labelId="demo-simple-select-label"
-                                id="demo-simple-select"
-                                value={type}
-                                label="Engraving"
-                                onChange={handleChangeDesign}
+                            {/* change this to be the different categories */}
+                            {type == 'icon' &&
+                                <Box className="flex justify-center md:w-1/6 mt-10 mr-0 lg:mr-4 px-2">
+                                    <FormControl fullWidth>
+                                        <InputLabel id="demo-simple-select-label">Categories</InputLabel>
+                                        <Select
+                                        labelId="demo-simple-select-label"
+                                        id="demo-simple-select"
+                                        value={category}
+                                        label="Categories"
+                                        onChange = {handleCategoryChange}
+                                        >
+                                            {Object.keys(traitletOptions['centerpiece']).map((category) => {
+                                                return <MenuItem value={category}>{formatString(category)}</MenuItem>
+                                            })}
+                                        </Select>
+                                    </FormControl>
+                                </Box>
+                            } 
+                            {type == 'text' && <div className='ml-8 mt-4'>
+                                <Box
+                                    component="form"
+                                    sx={{
+                                       
+                                    }}
+                                    noValidate
+                                    autoComplete="off"
                                 >
-                                    <MenuItem value={'icon'}>Icon</MenuItem>
-                                    <MenuItem value={'text'}>Text</MenuItem>
-                                    <MenuItem value={'none'}>None</MenuItem>
-                                </Select>
-                            </FormControl>
-                        </Box> 
-                        {type == 'text' && <div className='ml-8 mt-4'>
-                            <Box
-                                component="form"
-                                sx={{
-                                    '& > :not(style)': { m: 1, width: '25ch' },
-                                }}
-                                noValidate
-                                autoComplete="off"
-                            >
-                                {value == 0 ?
-                                    <TextField id="standard-basic" label="Engraving Text " variant="standard" value={engravingText} onChange={(event) => {handleEngravingChange(event, value)}}/>
-                                    :
-                                    <TextField id="standard-basic" label="Engraving Text " variant="standard" value={backEngravingText} onChange={(event) => {handleBackEngravingChange(event, value)}}/>
-                                }
-                            </Box>
+                                    {value == 0 ?
+                                        <TextField id="standard-basic" label="Engraving Text " variant="standard" value={engravingText} onChange={(event) => {handleEngravingChange(event, value)}}/>
+                                        :
+                                        <TextField id="standard-basic" label="Engraving Text " variant="standard" value={backEngravingText} onChange={(event) => {handleBackEngravingChange(event, value)}}/>
+                                    }
+                                </Box>
                         </div>}
                         
                         {/* fix this */}    
-                        {type == 'icon' && <div ref={iconContainerRef} className={`flex gap-8 mt-12 overflow-auto ${ !isSmallScreen && 'max-w-[60%]'}`}>
-                            {options.map((optionObj) => {
-                                return(
-                                    <div onClick={(event) => {event.stopPropagation(); addCenterpieceToOrder(optionObj);}}>
-                                        <OptionButton opt={optionObj} side={value}/>
-                                    </div>
-                                )
-                            })}
+                        {type == 'icon' && 
+                            <div ref={iconContainerRef} className={`flex gap-8 mt-12 overflow-auto ${ !isSmallScreen && 'max-w-[60%]'}`}>
+                                {options.map((optionObj) => {
+                                    return(
+                                        <div onClick={(event) => {event.stopPropagation(); addCenterpieceToOrder(optionObj);}}>
+                                            <OptionButton opt={optionObj} side={value}/>
+                                        </div>
+                                    )
+                                })}
 
-                        </div>}
+                            </div>}
                     </div>
                     </div>
                     
@@ -184,53 +220,3 @@ function SelectionsDisplay() {
 }
 
 export default SelectionsDisplay
-
-{/* <div className='flex justify-center'>
-<div className='flex md:flex-row justify-center '>
-    <Box>
-        <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label">Engraving</InputLabel>
-            <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={type}
-                label="Engraving"
-                onChange={type != 'none' ? handleChangeDesign : addCenterpieceToOrder(type)}
-            >
-                <MenuItem value={'icon'}>Icon</MenuItem>
-                <MenuItem value={'text'}>Text</MenuItem>
-                <MenuItem value={'none'}>None</MenuItem>
-            </Select>
-        </FormControl>
-    </Box> 
-    {type == 'text' && <div className='ml-8 mt-4 md:max-w-[100%] max-w-xs'>
-        <Box
-            component="form"
-            sx={{
-                '& > :not(style)': { m: 1, width: '100%' },
-            }}
-            noValidate
-            autoComplete="off"
-        >
-            {value == 0 ?
-                <TextField id="standard-basic" label="Engraving Text " variant="standard" value={engravingText} onChange={(event) => {handleEngravingChange(event, value)}}/>
-                :
-                <TextField id="standard-basic" label="Engraving Text " variant="standard" value={backEngravingText} onChange={(event) => {handleBackEngravingChange(event, value)}}/>
-            }
-        </Box>
-    </div>} */}
-
-    // {/* fix this */}
-    // {type == 'icon' && 
-    //     <div className='overflow-container overflow-auto flex md:gap-4 gap-2 lg:max-w-6xl md:max-w-4xl max-w-[7%] mt-10'>
-    //         {options.map((optionObj) => {
-    //             return(
-    //                 <div className='flex-shrink-0 border-transparent border-opacity-50 border-8' onClick={(event) => {event.stopPropagation(); addCenterpieceToOrder(optionObj);}}>
-    //                     <OptionButton opt={optionObj} side={value}/>
-    //                 </div>
-    //             )
-    //         })}
-    //     </div>
-    // }
-// </div>
-// </div>
